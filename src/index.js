3eb12a94f140c35e09788cc5c89079cfec1dabcf
Andrew Duong <duongandrews@gmail.com>
2019-10-25 10:41:15 -0400

    Removing conditionals from render in the index file

--- src/index.js
+++ src/index.js
@@ -15,8 +15,7 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>;
     }
@@ -24,6 +23,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
